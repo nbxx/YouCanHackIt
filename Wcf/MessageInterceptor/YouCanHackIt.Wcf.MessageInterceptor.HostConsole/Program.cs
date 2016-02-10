2c4eb6da55ed1540da461615c909be26521115e5
nbxx <askgy@msn.com>
2016-02-10 16:22:04 -0500

    1 [NEW] add host and client console

---
+++ Wcf/MessageInterceptor/YouCanHackIt.Wcf.MessageInterceptor.HostConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouCanHackIt.Wcf.MessageInterceptor.HostConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
